796094cdbe6cb2b8d3715010af5e6b44d0d1437f
Max <barreiro.fontela.max@gmail.com>
2026-06-14 16:23:08 +0300

    Initial Commit

---
+++ Vexta/VextaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VextaApp.swift
+//  Vexta
+//
+//  Created by MaxAdmin on 14.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct VextaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
