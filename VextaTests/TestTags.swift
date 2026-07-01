77a08545bf4eca30761e61b1e16b399c1dd91347
Max <barreiro.fontela.max@gmail.com>
2026-07-01 17:03:13 +0300

    [Test] Add Swift Testing tags for repository and datasource

---
+++ VextaTests/TestTags.swift
@@ -0,0 +1,14 @@
+//
+//  TestTags.swift
+//  VextaTests
+//
+//  Created by MaxAdmin on 30.06.2026.
+//
+
+import Testing
+
+extension Tag {
+    @Tag static var repository: Self
+    @Tag static var datasource: Self
+}
+
