ca9a69d5f3bbe7db024586798e19c174ef7a2ac6
Max <barreiro.fontela.max@gmail.com>
2026-07-01 17:09:04 +0300

    [Test] Add Repository tests

---
+++ VextaTests/Core/Data/Repositories/CartRepositoryTests.swift
@@ -0,0 +1,66 @@
+//
+//  CartRepositoryTests.swift
+//  VextaTests
+//
+//  Created by MaxAdmin on 30.06.2026.
+//
+
+import Foundation
+import Testing
+@testable import Vexta
+
+@Suite(.tags(.repository))
+struct CartRepositoryTests {
+    private let sut: CartRepositoryImpl
+    private let TTL: TimeInterval
+
+    init() {
+        TTL = 5
+        sut = RepositoryFactory().makeCartRepository()
+    }
+
+    @Test func repository_fetch_dataFlow_network_cache_invalidation_persistence() async throws {
+        let remoteDTO = try await sut.fetch(cartId: 1)
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        let decodedDomain = try decoder.decode(
+            CartDTO.self,
+            from: CartDTO.stubData
+        ).toDomain
+
+        #expect(remoteDTO == decodedDomain)
+
+        let cachedDomain = try await sut.fetch(cartId: 1)
+        #expect(cachedDomain == decodedDomain)
+        try await Task.sleep(for: .seconds(TTL + 0.1))
+        let persistentDomain = try await sut.fetch(cartId: 1)
+        #expect(persistentDomain == decodedDomain)
+    }
+
+    @Test func repository_fetchAll_dataFlow_network_cache_invalidation_persistence() async throws {
+        let remoteDTOs = try await sut.fetchAll()
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
+        let decodedDomains = try decoder.decode(
+            [CartDTO].self,
+            from: CartDTO.stubsData
+        ).map(\.toDomain)
+
+        #expect(remoteDTOs == decodedDomains)
+
+        let cachedDomains = try await sut.fetchAll()
+        #expect(
+            Set(cachedDomains) == Set(decodedDomains)
+        )
+        try await Task.sleep(for: .seconds(TTL + 0.1))
+        let persistentDomains = try await sut.fetchAll()
+        #expect(
+            Set(persistentDomains) == Set(decodedDomains)
+        )
+    }
+}
+
---
+++ VextaTests/Core/Data/Repositories/UserRepositoryTests.swift
@@ -0,0 +1,39 @@
+//
+//  UserRepositoryTests.swift
+//  VextaTests
+//
+//  Created by MaxAdmin on 30.06.2026.
+//
+
+import Foundation
+import Testing
+@testable import Vexta
+
+@Suite(.tags(.repository))
+struct UserRepositoryTests {
+    private let sut: UserRepositoryImpl
+    private let TTL: TimeInterval
+
+    init() {
+        TTL = 5
+        sut = RepositoryFactory().makeUserRepository()
+    }
+
+    @Test
+    func repository_fetch_dataFlow_network_cache_invalidation_persistence() async throws {
+        let remoteDTO = try await sut.fetch(userId: 1)
+
+        let decodedDomain = try JSONDecoder().decode(
+            UserDTO.self,
+            from: UserDTO.stubData
+        ).toDomain
+
+        #expect(remoteDTO == decodedDomain)
+
+        let cachedDomain = try await sut.fetch(userId: 1)
+        #expect(cachedDomain == decodedDomain)
+        try await Task.sleep(for: .seconds(TTL + 0.1))
+        let persistentDomain = try await sut.fetch(userId: 1)
+        #expect(persistentDomain == decodedDomain)
+    }
+}
