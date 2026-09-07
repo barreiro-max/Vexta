8e502771f76062a90ccb9618b5ef0d97cdc3cf4b
Max <barreiro.fontela.max@gmail.com>
2026-09-07 14:13:32 +0300

    [Environment] Add new case useFirebaseEmulator for EnvironmentVariables

--- Packages/Environment/Sources/Environment/EnvironmentVariables.swift
+++ Packages/Environment/Sources/Environment/EnvironmentVariables.swift
@@ -8,22 +8,26 @@
 import Foundation
 
 public enum EnvironmentVariables {
-    static var isUserPremium:       Bool { EnvironmentKey.isPremium.isActive }
-    static var isTestConfiguration: Bool { EnvironmentKey.isTesting }
+    public static var isUserPremium:       Bool { EnvironmentKey.isPremium.isActive }
+    public static var isTestConfiguration: Bool { EnvironmentKey.isTesting }
+    public static var isUseFirebaseEmulator: Bool { EnvironmentKey.isFirebaseEmulator.isActive }
 }
 
 fileprivate enum EnvironmentKey {
     case isPremium
+    case isFirebaseEmulator
 
     private var key: String {
         switch self {
         case .isPremium: "FORCE_PREMIUM"
+        case .isFirebaseEmulator: "FIREBASE_EMULATOR"
         }
     }
 
     private var expectedValue: String? {
         switch self {
         case .isPremium: "YES"
+        case .isFirebaseEmulator: "YES"
         }
     }
 
